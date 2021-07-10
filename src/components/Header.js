1a7d5e2d63a480e1c860e33596f4fe98b6c84f2a
Luis <luisyuburi@outlook.com>
2021-07-10 17:05:09 -0500

    fixing title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,10 @@ import PropTypes from "prop-types";
 const Header = ({ titulo }) => {
   return (
     <nav className="nav-wrapper light-blue darken-3">
-      <a href="#!" className="brand-logo center">
+      <p className="flow-text center">{titulo}</p>
+      {/* <a href="#!" className="flow-text brand-logo center">
         {titulo}
-      </a>
+      </a> */}
     </nav>
   );
 };
